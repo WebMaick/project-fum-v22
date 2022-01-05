9a7baae374dbfea21324af1cddbdb7fc5901b0b6
miguel angel <mickmiguel05@gmail.com>
2022-01-05 19:28:37 -0400

    Inicio de Proyecto

---
+++ README.md
@@ -0,0 +1,1 @@
+# Bienvenido a Frente de Unidad del Magisterio
---
+++ src/AppFum.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export const AppFum = () => {
+  return (
+    <div>
+      <h2>Bienvenido A FUM</h2>
+    </div>
+  )
+}
